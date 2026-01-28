62aad0a4454e3e4a5c6cdbf8ba2df6c5a24f3a23
cnerylozada <cnerylozada@gmail.com>
2026-01-28 11:36:44 -0500

    feat: evm onchain write

--- prediction-market/main.ts
+++ prediction-market/main.ts
@@ -1,12 +1,60 @@
 import {
   cre,
   decodeJson,
+  EVMClient,
+  getNetwork,
+  hexToBase64,
   HTTPCapability,
   HTTPPayload,
   Runner,
   type Runtime,
 } from "@chainlink/cre-sdk";
-import { ConfigType, configSchema, createMarketSchema } from "./models";
+import {
+  ConfigType,
+  CreateMarketSchemaType,
+  configSchema,
+  createMarketSchema,
+} from "./models";
+import { bytesToHex, encodeAbiParameters, parseAbiParameters } from "viem";
+
+const createNewStudent = (
+  runtime: Runtime<ConfigType>,
+  evmClient: EVMClient,
+  inputData: CreateMarketSchemaType,
+) => {
+  const { name, age } = inputData;
+  runtime.log(`name: ${name} age: ${age}`);
+
+  const reportData = encodeAbiParameters(
+    parseAbiParameters("string, uint256"),
+    [name as string, BigInt(age)],
+  );
+
+  const reportResponse = runtime
+    .report({
+      encodedPayload: hexToBase64(reportData),
+      encoderName: "evm",
+      signingAlgo: "ecdsa",
+      hashingAlgo: "keccak256",
+    })
+    .result();
+
+  const { gasLimit, marketConsumerAddress } = runtime.config.evm;
+  const writeReportResult = evmClient
+    .writeReport(runtime, {
+      receiver: marketConsumerAddress,
+      report: reportResponse,
+      gasConfig: {
+        gasLimit,
+      },
+    })
+    .result();
+  runtime.log("Waiting for write report response");
+
+  const txHash = bytesToHex(writeReportResult.txHash || new Uint8Array());
+  runtime.log(`View transaction at https://sepolia.etherscan.io/tx/${txHash}`);
+  return txHash;
+};
 
 const onHttpTrigger = (
   runtime: Runtime<ConfigType>,
@@ -20,8 +68,18 @@ const onHttpTrigger = (
   if (!inputData.success) {
     return "Error: invalid model";
   }
-  const { data } = inputData;
-  runtime.log(`inputData: ${data.question}`);
+  const network = getNetwork({
+    chainFamily: "evm",
+    chainSelectorName: runtime.config.evm.chainName,
+    isTestnet: true,
+  });
+  if (!network) {
+    return "Error: network not found";
+  }
+
+  const evmClient = new EVMClient(network.chainSelector.selector);
+  const txHash = createNewStudent(runtime, evmClient, inputData.data);
+  runtime.log(`txHash: ${txHash}`);
 
   return "Success";
 };
--- prediction-market/models.ts
+++ prediction-market/models.ts
@@ -1,6 +1,17 @@
 import { z } from "zod";
 
-export const configSchema = z.object({ authorizedEVMAddress: z.string() });
+export const configSchema = z.object({
+  authorizedEVMAddress: z.string(),
+  evm: z.object({
+    marketConsumerAddress: z.string(),
+    chainName: z.string(),
+    gasLimit: z.string(),
+  }),
+});
 export type ConfigType = z.infer<typeof configSchema>;
 
-export const createMarketSchema = z.object({ question: z.string() });
+export const createMarketSchema = z.object({
+  name: z.string(),
+  age: z.number().int().positive(),
+});
+export type CreateMarketSchemaType = z.infer<typeof createMarketSchema>;
